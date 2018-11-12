868b7bead196a7b95c39763aff3e9f176b411d3d
Djelali DINAUT <ddinaut@e1r12p21.42.fr>
2018-11-12 12:01:13 +0100

    norme

--- srcs/ft_nm/symbols.c
+++ srcs/ft_nm/symbols.c
@@ -6,29 +6,30 @@
 /*   By: ddinaut <ddinaut@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/26 16:07:49 by ddinaut           #+#    #+#             */
-/*   Updated: 2018/11/12 11:51:47 by ddinaut          ###   ########.fr       */
+/*   Updated: 2018/11/12 11:59:50 by ddinaut          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "nm.h"
 
-static void	push_symbol_chunk(unsigned long opt, t_symbol *new, t_symbol **symbol)
+static void	push_symbol_chunk(t_binary *bin, t_symbol *new, t_symbol **symbol)
 {
 	if (!new)
 		return ;
-	if (opt & FLAG_LP)
+	if (bin->opt & FLAG_LP)
 		no_sort(new, symbol);
-	else if (opt & FLAG_LN && opt & FLAG_LR)
+	else if (bin->opt & FLAG_LN && bin->opt & FLAG_LR)
 		rev_sort_numericaly(new, symbol);
-	else if (opt & FLAG_LN)
+	else if (bin->opt & FLAG_LN)
 		sort_numericaly(new, symbol);
-	else if (opt & FLAG_LR)
+	else if (bin->opt & FLAG_LR)
 		rev_alpha_sort(new, symbol);
 	else
 		alpha_sort(new, symbol);
 }
 
-static void	*resolve_symbol_name(t_binary *bin, struct stat stat, uint32_t stroff, uint32_t n_strx)
+static void	*resolve_symbol_name(t_binary *bin, struct stat stat, \
+								uint32_t stroff, uint32_t n_strx)
 {
 	void		*name;
 	uint32_t	roff;
@@ -40,36 +41,38 @@ static void	*resolve_symbol_name(t_binary *bin, struct stat stat, uint32_t strof
 	return (name);
 }
 
-int		parse_symbol_x32(struct symtab_command *symtab, struct nlist *list, t_binary *bin, struct stat stat)
+int			parse_symbol_x32(struct symtab_command *symtab, \
+							struct nlist *lt, t_binary *bin, struct stat stat)
 {
 	t_symbol *new;
 
 	if (!(new = (t_symbol*)malloc(sizeof(char) * sizeof(t_symbol))))
 		return (ERROR);
-	new->type = resolve_symbol_type(list->n_type, list->n_sect, list->n_value, bin);
+	new->type = resolve_symbol_type(lt->n_type, lt->n_sect, lt->n_value, bin);
 	new->fileoff = bin->offset;
-	new->value = reverse_32(bin->endian, list->n_value);
-	new->name = resolve_symbol_name(bin, stat, symtab->stroff, list->n_un.n_strx);
+	new->value = reverse_32(bin->endian, lt->n_value);
+	new->name = resolve_symbol_name(bin, stat, symtab->stroff, lt->n_un.n_strx);
 	new->next = NULL;
 	if (new->name == NULL)
 		return (ERROR);
-	push_symbol_chunk(bin->opt, new, &bin->sym);
+	push_symbol_chunk(bin, new, &bin->sym);
 	return (SUCCESS);
 }
 
-int		parse_symbol_x64(struct symtab_command *symtab, struct nlist_64 *list, t_binary *bin, struct stat stat)
+int			parse_symbol_x64(struct symtab_command *symtab, \
+							struct nlist_64 *lt, t_binary *bin, struct stat st)
 {
 	t_symbol *new;
 
 	if (!(new = (t_symbol*)malloc(sizeof(char) * sizeof(t_symbol))))
 		return (ERROR);
-	new->type = resolve_symbol_type(list->n_type, list->n_sect, list->n_value, bin);
+	new->type = resolve_symbol_type(lt->n_type, lt->n_sect, lt->n_value, bin);
 	new->fileoff = bin->offset;
-	new->value = reverse_64(bin->endian, list->n_value);
-	new->name = resolve_symbol_name(bin, stat, symtab->stroff, list->n_un.n_strx);
+	new->value = reverse_64(bin->endian, lt->n_value);
+	new->name = resolve_symbol_name(bin, st, symtab->stroff, lt->n_un.n_strx);
 	new->next = NULL;
 	if (new->name == NULL)
 		return (ERROR);
-	push_symbol_chunk(bin->opt, new, &bin->sym);
+	push_symbol_chunk(bin, new, &bin->sym);
 	return (SUCCESS);
 }
